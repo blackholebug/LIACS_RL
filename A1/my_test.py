8b11cc058029104da0cf2b7601d09a28d5083d89
blackholebug <blackholebug@gmail.com>
2020-02-17 22:29:19 +0100

    play with UI

--- A1/my_test.py
+++ A1/my_test.py
@@ -5,72 +5,69 @@ import typing
 
 INF = 99999
 
-class Node:
-    # Dict or Obj?
-    def __init__(self, coordinates, current_depth=0, search_depth=99990):
-        self.children = list()
-        self.position = coordinates
-        self.cdepth = current_depth
-        self.sdepth = search
-
-    def isLeaf(self):
-        return True if self.cdepth == self.sdepth else False
-
-    def isMax(self):
-        return True if self.cdepth % 2 == 0 else False
-
-    def isMin(self):
-        return True if self.cdepth % 2 != 0 else False
+def parse(c):
+    fil, rank = ord(c[0]) - ord('a'), int(c[1]) - 1
+    return A1 + fil - 10*rank
 
 
-# search-evaluate architecture
+def render(i):
+    rank, fil = divmod(i - A1, 10)
+    return chr(fil + ord('a')) + str(-rank + 1)
 
-def gen_move(hexboard: HexBoard, current_depth=0, search_depth: int=999999):
-    """
-    start randomly with RED, traverse the board
-    """
-    init_step = (random.randint(0, hexboard.size), random.randint(0, hexboard.size))
-    hexboard.place(init_step, hexboard.RED) 
-    root_node = Node(init_step, current_depth, search_depth)
-    move(hexboard, current_depth, search_depth, root_node)
 
-def move(hexboard: HexBoard, node: Node, current_depth=0, search_depth: int=999999):
-    """
-    search the board recursively
-    """
-    while not hexboard.is_game_over() and search_depth != current_depth:
-        current_depth += 1
-        for k, v in hexboard.board.items():
-            if v == hexboard.EMPTY:
-                current_step = k
+def print_pos(pos):
+    print()
+    uni_pieces = {'R':'♜', 'N':'♞', 'B':'♝', 'Q':'♛', 'K':'♚', 'P':'♟',
+                  'r':'♖', 'n':'♘', 'b':'♗', 'q':'♕', 'k':'♔', 'p':'♙', '.':'·'}
+    for i, row in enumerate(pos.board.split()):
+        print(' ', 8-i, ' '.join(uni_pieces.get(p, p) for p in row))
+    print('    a b c d e f g h \n\n')
+
+
+def main():
+    hist = [Position(initial, 0, (True,True), (True,True), 0, 0)]
+    searcher = Searcher()
+    while True:
+        print_pos(hist[-1])
+
+        if hist[-1].score <= -MATE_LOWER:
+            print("You lost")
+            break
+
+        # We query the user until she enters a (pseudo) legal move.
+        move = None
+        while move not in hist[-1].gen_moves():
+            match = re.match('([a-h][1-8])'*2, input('Your move: '))
+            if match:
+                move = parse(match.group(1)), parse(match.group(2))
+            else:
+                # Inform the user when invalid input (e.g. "help") is entered
+                print("Please enter a move like g8f6")
+        hist.append(hist[-1].move(move))
+
+        # After our move we rotate the board and print it again.
+        # This allows us to see the effect of our move.
+        print_pos(hist[-1].rotate())
+
+        if hist[-1].score <= -MATE_LOWER:
+            print("You won")
+            break
+
+        # Fire up the engine to look for a move.
+        start = time.time()
+        for _depth, move, score in searcher.search(hist[-1], hist):
+            if time.time() - start > 1:
                 break
-        current_color = hexboard.RED if current_depth % 2 == 0 else hexboard.BLUE
-        hexboard.place(current_step, current_color)
-        new_node = Node(current_step, current_depth, search_depth)
-        node.children.append(new_node)
-        move(hexboard, current_depth, search_depth, new_node)
 
-def eval(coordinates):
-    return random.random()
+        if score == MATE_UPPER:
+            print("Checkmate!")
 
-def alphabeta(node: Node, a: int, b: int, current_depth: int):
-    if current_depth <= 0 or node.isLeaf == True:
-        return eval(node.position)
-    elif node.isMax == True:  
-        g = -INF
-        for c in node.children:
-            g = max(g, alphabeta(c, a, b, current_depth-1))
-            a = max(a, g)
-            if g >= b:
-                break
-    elif node.isMin == True:
-        g = INF
-        for c in node.children:
-            g = min(g, alphabeta(c, a, b, current_depth-1))
-            b = min(b, g)
-            if a >= g:
-                break
-    return g
+        # The black player moves from a rotated position, so we have to
+        # 'back rotate' the move before printing it.
+        print("My move:", render(119-move[0]) + render(119-move[1]))
+        hist.append(hist[-1].move(move))
 
-def search():
-    return 0
+
+if __name__ == '__main__':
+    main()
+
