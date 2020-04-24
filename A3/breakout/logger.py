b512432db0ab35404f91bbb7738a2d3f49203bfd
blackholebug <blackholebug@gmail.com>
2020-04-24 18:52:48 +0200

    update log

--- A3/breakout/logger.py
+++ A3/breakout/logger.py
@@ -34,37 +34,45 @@ class Logger():
             print('{{"metric": "gameover", "value": {}}}'.format(gameover))
 
     def add_score(self, value):
-        self.add_log(self.score, value, "gameover", "score",
-                     RUN_UPDATE_FREQUENCY, self.directory_path, self.header)
+        self.score.append(value)
+        if len(self.score) % RUN_UPDATE_FREQUENCY == 0:
+            self.save_log(self.score, "gameover", "score",
+                          RUN_UPDATE_FREQUENCY, self.directory_path, self.header)
+            self.score = []
 
     def add_step(self, value):
-        self.add_log(self.step, value, "gameover", "number of steps",
-                     RUN_UPDATE_FREQUENCY, self.directory_path, self.header)
+        self.step.append(value)
+        if len(self.step) % RUN_UPDATE_FREQUENCY == 0:
+            self.save_log(self.step, "gameover", "number of steps",
+                          RUN_UPDATE_FREQUENCY, self.directory_path, self.header)
+            self.step = []
 
     def add_accuracy(self, value):
-        self.add_log(self.accuracy, value, "update", "accuracy",
-                     TRAINING_UPDATE_FREQUENCY, self.directory_path, self.header)
+        self.accuracy.append(value)
+        if len(self.accuracy) % TRAINING_UPDATE_FREQUENCY == 0:
+            self.save_log(self.accuracy, "update", "accuracy",
+                          TRAINING_UPDATE_FREQUENCY, self.directory_path, self.header)
+            self.accuracy = []
 
     def add_loss(self, value):
         value = min(5, value)  # clip loss, max = 5
-        self.add_log(self.loss, value, "update", "loss",
-                     TRAINING_UPDATE_FREQUENCY, self.directory_path, self.header)
+        self.loss.append(value)
+        if len(self.loss) % TRAINING_UPDATE_FREQUENCY == 0:
+            self.save_log(self.loss, "update", "loss",
+                          TRAINING_UPDATE_FREQUENCY, self.directory_path, self.header)
+            self.loss = []
 
-    def add_log(self, values, value, x_label, y_label, update_frequency, directory_path, header):
-        values.append(value)
-        if len(values) % update_frequency == 0:
-            mean_value = np.mean(values)
-            print(y_label + ": (min: " + str(min(values)) + ", avg: " +
-                  str(mean_value) + ", max: " + str(max(values)))
-            print('{"metric": "' + y_label + '", "value": {}}}'.format(mean_value))
-            self.save_data(self.directory_path + y_label + ".csv", mean_value)
-            self.save_fig(input_path=self.directory_path + y_label + ".csv",
-                          output_path=self.directory_path + y_label + ".png",
-                          small_batch_length=update_frequency,
-                          big_batch_length=update_frequency*10,
-                          x_label=x_label,
-                          y_label=y_label)
-            values = []
+    def save_log(self, values, x_label, y_label, update_frequency, directory_path, header):
+        mean_value = np.mean(values)
+        print(y_label + ": (min: " + str(min(values)) + ", avg: " +
+              str(mean_value) + ", max: " + str(max(values)))
+        print('{"metric": "' + y_label + '", "value": {}}}'.format(mean_value))
+        self.save_data(self.directory_path + y_label + ".csv", mean_value)
+        self.save_fig(input_path=self.directory_path + y_label + ".csv",
+                      output_path=self.directory_path + y_label + ".png",
+                      small_batch_length=update_frequency,
+                      x_label=x_label,
+                      y_label=y_label)
 
     def save_fig(self, input_path, output_path, small_batch_length, big_batch_length, x_label, y_label):
         x = []
